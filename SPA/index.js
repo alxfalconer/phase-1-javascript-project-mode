c81f173309698d51679dc790d5dfda9ff3c343b9
Alexander <al.x.falconer@gmail.com>
2021-05-10 11:31:41 -0400

    refactor

--- SPA/index.js
+++ SPA/index.js
@@ -1,4 +1,4 @@
-const url = 'http://brianeno.needsyourhelp.org/draw'
+// declare and initialize constant variables using document method
 
 const cardText = document.getElementById('card-text'),
         userText = document.getElementById('user-text'),
@@ -6,7 +6,8 @@ const cardText = document.getElementById('card-text'),
         genCardBtn = document.getElementById('card-btn'),
         clearBtn = document.getElementById('clear-user'),
         userBtn = document.getElementById('user-btn'),
-        saveBtn = document.getElementById('save-btn');
+        saveBtn = document.getElementById('save-btn'),
+        url = 'http://brianeno.needsyourhelp.org/draw';
 
 
 function randomCard() {
@@ -29,7 +30,7 @@ function clearText() {
 
 function clearUser() {
     userData.innerHTML = "";
-    sessionStorage.clear();
+    window.localStorage.clear(userData);
 }
 
 function learnMore() {
@@ -50,12 +51,10 @@ function errorMessage() {
 }
 
 class DataSystem {
-    // constructor function is ran when the class is called.
     constructor() {
       this.dataStore = [];
     }
-  
-    // made class method to add a new data object to data array.
+
     add({ problem, solution }){
       this.dataStore.push({
         problem,
@@ -63,59 +62,36 @@ class DataSystem {
       });
     }
   }
-  
-  // making a new data system
+
   const dataStore = new DataSystem();
   
-  // adding a click event to the add button, so when you click it it adds the data to the data system and re-renders the DOM.
-  //document.querySelector("button").addEventListener("click", () => saveData());
-  
   function saveData() {
-    // using add method to add new data.
     dataStore.add({
-      // selecting the value of the element with id of problem;
       problem: userText.value,
-      // selecting the value of the element with id of solution;
       solution: cardText.innerText,
     })
-    // rerendering the data list in the DOM
     displayData();
   }
   
-  // loops through the data and adds them to dom.
   function displayData() {
-    // selecting element with id of data
     const parent = userData;
-    // clearing it so it has noting inside.
-    // do this so we don't have duplicate data.
     parent.innerHTML = "";
     
-    // looping through each data in the data system.
-    dataStore.dataStore.forEach(t => {
-      // making a new div element.
+    dataStore.dataStore.map(data => {
       const elm = document.createElement("div");
-      // giving it a class of data
       elm.classList.add("user-data");
       
-      // making a new div element.
       const prob = document.createElement("div");
-      // giving it a class of problem
       prob.classList.add("user-text");
-      //setting it's text value to the current data problem;
-      prob.textContent = t.problem;
+      prob.textContent = data.problem;
       
-      // making a new div element.
       const solu = document.createElement("div");
-      // giving it a class of solution
       solu.classList.add("card-text");
-      //setting it's text value to the current data solution;
-      solu.textContent = t.solution;
-      
-      // adding the problem and solution div to the data div.
+      solu.textContent = data.solution;
+
       elm.appendChild(prob);
       elm.appendChild(solu);
       
-      // adding the ticket div to the tickets div.
       parent.appendChild(elm);
     });
   }
